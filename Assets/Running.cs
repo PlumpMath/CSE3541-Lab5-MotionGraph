cd1367ff0b73800f1500de06e848cb62aa464685
Kurt Mueller <kurtronaldmueller@gmail.com>
2016-04-01 21:50:01 -0400

    Implement basic run

---
+++ Assets/Running.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Running : MonoBehaviour {
+
+    Animator runAnimator;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        runAnimator = GetComponent<Animator>();
+
+        float vert = Input.GetAxis("Vertical");
+
+        runAnimator.SetFloat("speed", vert);
+
+        Debug.Log(vert);
+    }
+}
